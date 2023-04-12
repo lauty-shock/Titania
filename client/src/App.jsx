05a25d53f3f75f792e440316670947266ebe38ad
lauty-shock <lautaroperez135@gmail.com>
2023-04-12 07:58:59 -0300

    Card creada

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
 import Landing from "./components/Landing";
-import Marcas from "./components/Marcas";
+import Card from "./components/Card";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const zapa = {
+    name: "ZAPATILLAS DE RUNNING ADIDAS ADIZERO SL",
+    price: "54.999,00",
+    category: "Hombre Running",
+    colors: [
+      "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/877f87fbcbf34e299720aef600eff064_9366/ZAPATILLAS_DE_RUNNING_ADIDAS_ADIZERO_SL_Negro_HQ1349_01_standard.jpg",
+      "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/96fa7303d14249b7af33af9c00a79c5c_9366/ZAPATILLAS_DE_RUNNING_ADIDAS_ADIZERO_SL_Naranja_GX9775_01_standard.jpg",
+      "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/47c4f74efd14441ea551aef600ef8e74_9366/ZAPATILLAS_DE_RUNNING_ADIDAS_ADIZERO_SL_Azul_HQ1345_01_standard.jpg",
+      "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/626aefae60f647a496e6aef900bab3bf_9366/Zapatillas_De_Running_Adidas_Adizero_SL_Blanco_GV9095_01_standard.jpg",
+    ],
+  };
   return (
     <div className="App">
       <Navbar />
       <Landing />
-      <Marcas />
+      <Card product={zapa} />
     </div>
   );
 }
---
+++ client/src/components/Card.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import css from "../css/Card/Card.module.css";
+
+export default function Card({ product }) {
+  const [image, setImage] = useState(product.colors[0]);
+  return (
+    <div className={css.card}>
+      <img src={image} alt={product.name} />
+      <p className={css.price}>$ {product.price}</p>
+      <div className={css.colors}>
+        {product.colors.map((p, index) => (
+          <img
+            onMouseOver={() => setImage(p)}
+            onMouseOut={() => setImage(product.colors[0])}
+            key={index}
+            src={p}
+            alt={product.name}
+          />
+        ))}
+      </div>
+      <span className={css.title}>{product.name}</span>
+      <span>{product.category}</span>
+      <span>{product.colors.length} colores</span>
+    </div>
+  );
+}
